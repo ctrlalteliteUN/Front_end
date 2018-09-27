0255cc0dd63572e35ad7557181084c3824f5295c
Diego Duarte <dduartec@unal.edu.co>
2018-09-26 20:07:55 -0500

    Done Login
    Login finalizado

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,18 +2,60 @@ import React, { Component } from 'react';
 import '../styles/Log.css';
 import '../styles/App.css';
 import Navigation from './NavigationLog';
-import logo from '../resources/kc.png';
 
 class Login extends Component {
   render() {
     return (
       <div>
         <Navigation/>
-        <div className="container">
-          <div  className="container-login">
+          <div  className="body-login">
+            <div className="container">
+            <div className="container container-login">
+              <div className="row">
+                <div className="col-sm-12 log-text">
+                <h2>Ingresa</h2>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-8 offset-sm-2 myform-cont">
+                  <form role="form" actions="" method="post" className="">
+                    <div className="form-group">
+                      <input type="text" name="form-username" placeholder="Usuario" className="form-control" id="form-username"/>
+                    </div>
+                    <div className="form-group">
+                      <input type="password" name="form-password" placeholder="Contraseña" className="form-control" id="form-password"/>
+                    </div>
+                    <button type="submit" className="mybtn">Login</button>
+                  </form>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-12 mysocial-login log-text">
+                  <h3> Ó ingresa con: </h3>                  
+                  <div className="mysocial-login-buttons">
+                    <a className="mybtn-social">
+                      <i className="fab fa-google"></i>
+                    </a>
+                    <a className="mybtn-social">
+                      <i className="fab fa-facebook-f"></i>
+                    </a>                    
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-12 log-text">
+                <hr></hr>
+                </div>
+                <div className="col-sm-8 offset-sm-2 myform-cont">
+                  <form role="form" actions="" method="post" className="">
+                <button type="submit" className="mybtn">Registrate</button>
+                  </form>
+                  </div>
+              </div>
+            </div>
+            </div>
           </div>
         </div>
-      </div>
     );
   }
 }
